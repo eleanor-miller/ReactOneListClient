7fe6255671eea05b00e5efa3c21378bd5dc39ce4
eleanor-miller <eleanormiller08@icloud.com>
2021-12-08 17:41:13 -0500

    added a state to track the new value of a todo item

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ type TodoItemType = {
 
 export function App() {
   const [todoItems, setTodoItems] = useState<TodoItemType[]>([])
+  const [newTodoText, setNewTodoText] = useState('')
 
   // useEffect has a non-async function
   useEffect(function () {
@@ -51,7 +52,14 @@ export function App() {
           })}
         </ul>{' '}
         <form>
-          <input type="text" placeholder="Whats up?" />
+          <input
+            type="text"
+            placeholder="Whats up?"
+            value={newTodoText}
+            onChange={function (event) {
+              setNewTodoText(event.target.value)
+            }}
+          />
         </form>
       </main>
       <footer>
